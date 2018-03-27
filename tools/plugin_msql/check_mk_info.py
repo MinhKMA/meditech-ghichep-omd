89e8513c9a1418ccc8158946ca964b1297048cf6
Nguyễn Văn Minh <nguyenvanminhkma@gmail.com>
2018-03-27 19:03:19 +0700

    Create check_mk_info.py

---
+++ tools/plugin_msql/check_mk_info.py
@@ -0,0 +1,8 @@
+#!/root/environments/minhkma/bin/python3.5
+
+def perfometer_check_mk_info(row, check_command, perf_data):
+    number = int(perf_data[0][1])
+    color = "#00ff00"
+    return number, perfometer_linear(100,color)
+
+perfometers['check_mk-check_mk_info'] = perfometer_check_mk_info
